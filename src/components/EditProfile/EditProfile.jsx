4a282ebd2cb4e82e3c7fbf68e966698dfe773004
nirmal100-ux <nirmalstha783@gmail.com>
2023-08-23 09:30:53 +0545

    change

--- src/components/EditProfile/EditProfile.jsx
+++ src/components/EditProfile/EditProfile.jsx
@@ -53,7 +53,7 @@ const EditProfile = ({ setOpen }) => {
         // Upload completed successfully, now we can get the download URL
         getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
           try {
-            const updateProfile = await axios.put(`/users/${currentUser._id}`, {
+            const updateProfile = await axios.put(`https://server-wvye.onrender.com/api/users/${currentUser._id}`, {
               profilePicture: downloadURL,
             });
 
@@ -70,7 +70,7 @@ const EditProfile = ({ setOpen }) => {
   };
 
   const handleDelete = async () => {
-    const deleteProfile = await axios.delete(`/users/${currentUser._id}`);
+    const deleteProfile = await axios.delete(`https://server-wvye.onrender.com/api/users/${currentUser._id}`);
     dispatch(logout());
     navigate("/signin");
   };
--- src/pages/Signin/Signin.jsx
+++ src/pages/Signin/Signin.jsx
@@ -18,7 +18,7 @@ const Signin = () => {
     e.preventDefault();
     dispatch(loginStart());
     try {
-      const res = await axios.post("/auth/signin", { username, password });
+      const res = await axios.post("https://server-wvye.onrender.com/api/auth/signin", { username, password });
       dispatch(loginSuccess(res.data));
       navigate("/");
     } catch (err) {
